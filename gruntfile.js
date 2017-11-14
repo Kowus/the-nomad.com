ab156393b4c2f8688105e6716cd9e8df5ca5f7b0
Kowus <barnabas@elite-education.org>
2017-11-14 01:48:17 +0000

    automated npm

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,26 @@
 /*
  * Created by barnabasnomo on 11/13/17 at 9:23 PM
 */
+
+module.exports = (grunt) => {
+    grunt.initConfig({
+        cssmin: {
+            target: {
+                files: {
+                    'public/stylesheets/dist/bundle.min.css': ['public/stylesheets/src/base.css', 'public/stylesheets/src/main.css', 'public/stylesheets/src/vendor.css']
+                    }
+            }
+        },watch:{
+            css:{
+                files:['public/stylesheets/src/*.css'],
+                tasks:['css']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('css',['cssmin']);
+    grunt.registerTask('default', ['cssmin'])
+};
