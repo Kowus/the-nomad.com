90eae082bbe0d99368f4b261fabfa27980d303f7
Kowus <barnabas@elite-education.org>
2017-11-15 04:46:23 +0000

    test litmus

--- gruntfile.js
+++ gruntfile.js
@@ -1,33 +1,43 @@
 /*
  * Created by barnabasnomo on 11/13/17 at 9:23 PM
 */
-
 module.exports = (grunt) => {
     grunt.initConfig({
         cssmin: {
             target: {
                 files: {
                     'public/stylesheets/dist/bundle.min.css': ['public/stylesheets/src/base.css', 'public/stylesheets/src/main.css', 'public/stylesheets/src/vendor.css']
-                    }
+                }
             }
-        },watch:{
-            css:{
-                files:['public/stylesheets/src/*.css'],
-                tasks:['css']
-            },scripts:{
-                files:['public/javascripts/src/*.js'],
-                tasks:['js']
-            },configFiles: {
-                files: [ 'gruntfile.js', 'config/*.js' ],
+        },
+        watch: {
+            css: {
+                files: ['public/stylesheets/src/*.css'],
+                tasks: ['css']
+            }, scripts: {
+                files: ['public/javascripts/src/*.js'],
+                tasks: ['js']
+            }, configFiles: {
+                files: ['gruntfile.js', 'config/*.js'],
                 options: {
                     reload: true
                 }
             }
         },
-        uglify:{
-            my_target:{
-                files:{
-                    'public/javascripts/dist/bundle.min.js':['public/javascripts/src/jquery-1.11.3.min.js','public/javascripts/src/jquery-migrate-1.2.1.min.js','public/javascripts/src/plugins.js','public/javascripts/src/main.js']
+        uglify: {
+            my_target: {
+                files: {
+                    'public/javascripts/dist/bundle.min.js': ['public/javascripts/src/jquery-1.11.3.min.js', 'public/javascripts/src/jquery-migrate-1.2.1.min.js', 'public/javascripts/src/plugins.js', 'public/javascripts/src/main.js']
+                }
+            }
+        },litmus: {
+            test: {
+                src: ['views/email/new_post.hbs'],
+                options: {
+                    username: 'email',
+                    password: 'password',
+                    url: 'https://subdomain.litmus.com',
+                    clients: ['gmailnew', 'ffgmailnew', 'chromegmailnew']
                 }
             }
         }
@@ -36,9 +46,11 @@ module.exports = (grunt) => {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-litmus');
+
 
     grunt.registerTask('js', ['uglify']);
-    grunt.registerTask('css',['cssmin']);
-    grunt.registerTask('bundle',['css','js']);
+    grunt.registerTask('css', ['cssmin']);
+    grunt.registerTask('bundle', ['css', 'js']);
     grunt.registerTask('default', ['bundle'])
 };
