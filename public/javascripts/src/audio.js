64d35a8ab49ff1b74ed3cf9b55e4bcf063771b0c
Kowus <barnabas@elite-education.org>
2017-11-15 19:01:32 +0000

    added manual position set

--- public/javascripts/src/audio.js
+++ public/javascripts/src/audio.js
@@ -7,10 +7,10 @@ function progressBar() {
     var elapsedTime = Math.round(oAudio.currentTime);
     var prog = document.getElementsByClassName('playing')[0].getElementsByClassName('podcast-elapsed-bar')[0];
     var pld = document.getElementsByClassName('playing')[0].getElementsByClassName('played-position')[0];
-    var seek =document.getElementsByClassName('playing')[0].getElementsByClassName('seek')[0];
+    var seek = document.getElementsByClassName('playing')[0].getElementsByClassName('seek')[0];
     var pWidth = (elapsedTime / oAudio.duration) * 100;
     prog.style.width = pWidth + "%";
-    seek.style.left = pWidth -50 +"%";
+    seek.style.left = pWidth - 50 + "%";
     pld.value = elapsedTime;
 }
 
@@ -20,9 +20,9 @@ function playAudio(domEl) {
             var oAudio = document.getElementById('myaudio'),
                 audioUrl = domEl.getElementsByClassName('audio-src')[0]
             ;
-            if (oAudio.src.toString() != window.location.origin.toString()+audioUrl.value.toString()) {
+            if (oAudio.src.toString() != window.location.origin.toString() + audioUrl.value.toString()) {
                 oAudio.src = audioUrl.value;
-                if(document.getElementsByClassName('playing')[0]) {
+                if (document.getElementsByClassName('playing')[0]) {
                     document.getElementsByClassName('playing')[0].getElementsByClassName('play-podcast')[0].classList.add('ion-play');
                     document.getElementsByClassName('playing')[0].getElementsByClassName('play-podcast')[0].classList.remove('ion-pause');
                     document.getElementsByClassName('playing')[0].classList.remove('playing');
@@ -53,19 +53,21 @@ function initEvents() {
     var oAudio = document.getElementById('myaudio');
 
     oAudio.addEventListener('timeupdate', progressBar, true);
-    oAudio.addEventListener('ended',function () {
+    oAudio.addEventListener('ended', function () {
         var prog = document.getElementsByClassName('playing')[0].getElementsByClassName('podcast-elapsed-bar')[0];
-        var seek =document.getElementsByClassName('playing')[0].getElementsByClassName('seek')[0];
+        var seek = document.getElementsByClassName('playing')[0].getElementsByClassName('seek')[0];
         document.getElementsByClassName('playing')[0].getElementsByClassName('play-podcast')[0].classList.add('ion-play');
         document.getElementsByClassName('playing')[0].getElementsByClassName('play-podcast')[0].classList.remove('ion-pause');
         var pWidth = 0;
         prog.style.width = pWidth + "%";
-        seek.style.left = pWidth -50 +"%";
+        seek.style.left = pWidth - 50 + "%";
     });
     // Click Handler for each
     [].forEach.call(document.getElementsByClassName('play-podcast'), function (el) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
         var pane = el.parentNode.parentNode.parentElement;
         var progress = pane.getElementsByClassName('podcast-progress')[0];
+        var seek = pane.getElementsByClassName('seek')[0];
         el.addEventListener('click', function (e) {
             if (!e) {
                 e = window.event;
@@ -85,13 +87,63 @@ function initEvents() {
                 e = window.event;
             }
             try {
+                console.log(e);
                 oAudio.currentTime = oAudio.duration * (e.offsetX / progress.offsetWidth)
             } catch (err) {
                 catcher(err);
             }
 
-        },true)
+        }, true);
 
+        seek.onmousedown = dragMouseDown;
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            var oAudio = document.getElementById('myaudio');
+            var pld = pane.getElementsByClassName('played-position')[0];
+            var prog = pane.getElementsByClassName('podcast-elapsed-bar')[0];
+            var seek = pane.getElementsByClassName('seek')[0];
+            e = e || window.event;
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            // elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            oAudio.pause();
+            pld.value = Math.round(oAudio.duration * ((e.offsetX - pos1) / progress.offsetWidth));
+            oAudio.currentTime = oAudio.duration * ((e.offsetX - pos1) / progress.offsetWidth);
+            var pWidth = (Math.round(oAudio.currentTime / oAudio.duration) )* 100;
+            prog.style.width = pWidth + "%";
+            seek.style.left = pWidth - 50 + "%";
+        }
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function closeDragElement() {
+            /* stop moving when mouse button is released:*/
+            oAudio.play();
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
     });
 }
 
@@ -100,3 +152,38 @@ function catcher(e) {
 }
 
 window.addEventListener("DOMContentLoaded", initEvents, false);
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    elmnt.onmousedown = dragMouseDown;
+
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        // elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
