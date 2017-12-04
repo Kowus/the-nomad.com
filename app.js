8b02c010f547118a7e124695f7d0236a3eb0b8a7
Kowus <barnabas@elite-education.org>
2017-12-04 04:51:14 +0000

    generic error stack

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {status:err.status||500, message:err.message||'Sorry, an error occurred while performing the requested operation. Please try again later.'};
+    res.locals.error = req.app.get('env') === 'development' ? err : {status:err.status||500, message:err.message||'Not Found.',stack:`Requested resource not found: ${req.url}`};
 
     // render the error page
     res.status(err.status || 500);
