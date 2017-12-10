ab06f01171d3d996c7a35d265312e6caab89692c
Kowus <barnabas@elite-education.org>
2017-12-10 10:44:47 +0000

    update subscription list

---
+++ models/subscribe.js
@@ -0,0 +1,16 @@
+/*
+ * the-nomad.com ==> subscribe
+ * Created By barnabasnomo on 12/8/17 at 12:29 PM
+ * @soundtrack Juicy - Notorious BIG
+*/
+
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema
+;
+
+let Subscriber = new Schema({
+    email: String,
+    confirmed: true
+});
+
+module.exports = mongoose.model('Subscriber', Subscriber);
