0131d8746cf4ebebda8da7fb3494108924b59e5b
Kowus <barnabas@elite-education.org>
2017-12-29 13:53:39 +0000

    resort to serving local sitemap xml file

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use((req, res, next)=>{
 });
 
 app.use('/', routes);
-app.use('/sitemap.xml', require('./config/sitemap'));
+// app.use('/sitemap.xml', require('./config/sitemap'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
